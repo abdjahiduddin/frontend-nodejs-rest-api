52933f5a500f5ea0c1f251d6d9be1f84e4ead1d3
abd.jahiduddin@gmail.com <abd.jahiduddin@gmail.com>
2021-12-11 15:25:34 +0800

    Updates

---
+++ precache-manifest.f4647a5b7836f05c80b36beb355e7bb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26d9fa5ad8c186670947",
+    "url": "/frontend-nodejs-rest-api/static/css/main.82d09a87.chunk.css"
+  },
+  {
+    "revision": "26d9fa5ad8c186670947",
+    "url": "/frontend-nodejs-rest-api/static/js/main.26d9fa5a.chunk.js"
+  },
+  {
+    "revision": "ed9e244212a7c6b7c49b",
+    "url": "/frontend-nodejs-rest-api/static/js/1.ed9e2442.chunk.js"
+  },
+  {
+    "revision": "5f3ba41f30c8ecc06d2e",
+    "url": "/frontend-nodejs-rest-api/static/js/runtime~main.5f3ba41f.js"
+  },
+  {
+    "revision": "658222e6c6182ad9b9b3da7d9d1a908f",
+    "url": "/frontend-nodejs-rest-api/index.html"
+  }
+];
